4b4d9bb69fc00f049cfd64a79f69547ddd9fa276
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2025-01-25 22:52:13 +0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -14,3 +14,21 @@ function getTimeString(time){
     return `${year}year ${month}month  ${day} day ${hour} hour ${minute} minute  ${remainingSecond} second ago`
 }
 console.log(getTimeString(464626))
+
+
+/** 
+    1. load and display data initially.
+    2. onclick load and display data.
+    3. Complex object accessing.
+    4. toggle active class functionality.
+    5. search functionality.
+    6. load & display single data.
+    7. display data in a modal.
+    8. load & display single category data.
+    9. Conditional rendering if array dont have any data
+    10.Conditional rendering :: if a value empty
+    11.Conditional rendering :: if a value length is zero
+
+
+
+*/
